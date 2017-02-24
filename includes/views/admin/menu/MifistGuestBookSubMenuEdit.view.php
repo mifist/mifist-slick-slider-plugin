c64b2bc012da9dba81aa269fffcce223f4f07274
mifist <avanturka1992@gmail.com>
2017-02-24 04:22:50 +0200

    Создание Goest Book, минимальное задание

--- includes/views/admin/menu/MifistGuestBookSubMenuEdit.view.php
+++ includes/views/admin/menu/MifistGuestBookSubMenuEdit.view.php
@@ -2,10 +2,10 @@
 
 <!-- View форма для редактирования записи в таблицу. action формы это ссылка на страницу гостевой книги с $_GET['action']
 параметр &action=update_data в методе render контроллера мы будем обрабатывать параметр $_GET['action']  update_data.
-Эта форма похожая на форму StepByStepGuestBookSubMenuAdd.view.php только у ее полей ввода есть атрибут value со значением
+Эта форма похожая на форму MifistGuestBookSubMenuAdd.view.php только у ее полей ввода есть атрибут value со значением
 записи в таблицы которую мы будем редактировать. И еще есть одно скрытое поле id по котором будем обновлять запись в таблице.
 -->
-<form action="admin.php?page=step_by_step_control_guest_book_menu&action=update_data" method="post">
+<form action="admin.php?page=mifist_control_guest_book_menu&action=update_data" method="post">
 <input type="text" name="user_name" value="<?php echo $data['user_name']; ?>">
 <textarea name="message">
 	<?php echo $data['message']; ?>
@@ -13,5 +13,5 @@
 <!-- Поле id по котором будем обновлять запись в таблице -->
 <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
 
-<input type="submit" name="<?php _e('Add', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>">
+<input type="submit" name="<?php _e('Add', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>">
 </form>
