00856b8b4b278b9444b0511875c9573318a315c2
mifist <avanturka1992@gmail.com>
2017-02-24 04:22:10 +0200

    Создание Goest Book, минимальное задание

--- includes/views/admin/menu/MifistGuestBookSubMenu.view.php
+++ includes/views/admin/menu/MifistGuestBookSubMenu.view.php
@@ -1,24 +1,24 @@
 <!-- Ссылка ссылаеться на страницу гостевой книги только у нее добавлен $_GET['action'] параметр &action=add_data
     По этому параметру мы будем в методе render определять что делать
  -->
-<a href="admin.php?page=step_by_step_control_guest_book_menu&action=add_data">
-    <?php _e('Add', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+<a href="admin.php?page=mifist_control_guest_book_menu&action=add_data">
+    <?php _e('Add', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
 </a>
 
 <table  border="1">
     <thead>
     <tr>
         <td>
-            <?php _e('Name', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+            <?php _e('Name', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
         </td>
         <td>
-            <?php _e('Messsage', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+            <?php _e('Messsage', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
         </td>
         <td>
-            <?php _e('Date', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+            <?php _e('Date', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
         </td>
         <td>
-            <?php _e('Actions', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+            <?php _e('Actions', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
         </td>
     </tr>
     </thead>
@@ -43,11 +43,11 @@
                      для редактирования &action=edit_data для удаления &action=delete_data и в этих ссылок еще добавлен
                      один $_GET['id'] параметр это &id=(id записи) записи гостевой книги по котором мы будем выполнять
                      действия -->
-                    <a href="admin.php?page=step_by_step_control_guest_book_menu&action=edit_data&id=<?php echo $value['id'];?>">
-                        <?php _e('Edit', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+                    <a href="admin.php?page=mifist_control_guest_book_menu&action=edit_data&id=<?php echo $value['id'];?>">
+                        <?php _e('Edit', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
                     </a>
-                    <a href="admin.php?page=step_by_step_control_guest_book_menu&action=delete_data&id=<?php echo $value['id'];?>">
-                        <?php _e('Delete', STEPBYSTEP_PlUGIN_TEXTDOMAIN ); ?>
+                    <a href="admin.php?page=mifist_control_guest_book_menu&action=delete_data&id=<?php echo $value['id'];?>">
+                        <?php _e('Delete', MIFISTSLICK_PlUGIN_TEXTDOMAIN ); ?>
                     </a>
 
 
