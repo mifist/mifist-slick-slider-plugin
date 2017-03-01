ed86ed6b9deb73cf934ab41b78343baa18d394ec
mifist <avanturka1992@gmail.com>
2017-03-01 07:18:46 +0200

    Создание и вывод Post Type

---
+++ includes/custom_post_type/PortfolioPostType.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: avant
+ * Date: 28.02.17
+ * Time: 21:18
+ */
---
+++ includes/views/site/pages/MifistPortfolio.view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: avant
+ * Date: 01.03.17
+ * Time: 1:18
+ */
